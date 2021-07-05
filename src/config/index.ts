37a158316b22619d3ab20e222a706b08e2e65692
sliu7714 <siyuanliu3@gmail.com>
2021-07-05 16:51:28 -0600

    Changed block time and cake per block variables to match GIVE token

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const BSC_BLOCK_TIME = 3
+export const BSC_BLOCK_TIME = 2
 
 // CAKE_PER_BLOCK details
 // 40 CAKE is minted per block
@@ -16,7 +16,7 @@ export const BSC_BLOCK_TIME = 3
 // CAKE_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // CAKE/Block in src/views/Home/components/CakeStats.tsx = 20 (40 - Amount sent to burn pool)
 
-export const CAKE_PER_BLOCK = new BigNumber(40)
+export const CAKE_PER_BLOCK = new BigNumber(0.003)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const CAKE_PER_YEAR = CAKE_PER_BLOCK.times(BLOCKS_PER_YEAR)
 export const BASE_URL = 'https://pancakeswap.finance'
