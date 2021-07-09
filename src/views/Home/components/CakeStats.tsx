81094d370e01f3c15b451035d2ec3b3c4ba8b182
sliu7714 <siyuanliu3@gmail.com>
2021-07-09 11:01:01 -0600

    updated Cake stats card

--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Card, CardBody, Heading, Text } from '@pancakeswap/uikit'
+import BigNumber from 'bignumber.js/bignumber'
 import styled from 'styled-components'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
 import { useTranslation } from 'contexts/Localization'
-import { getCakeAddress } from 'utils/addressHelpers'
+import { getGiveAddress } from 'utils/addressHelpers'
 import CardValue from './CardValue'
+import {useFarmFromPid, useFarmGivePerBlock, useFarms, usePriceCakeBusd} from "../../../state/hooks";
+import {CAKE_PER_BLOCK} from "../../../config";
 
 const StyledCakeStats = styled(Card)`
   margin-left: auto;
@@ -24,26 +27,46 @@ const Row = styled.div`
 const CakeStats = () => {
   const { t } = useTranslation()
   const totalSupply = useTotalSupply()
-  const burnedBalance = getBalanceNumber(useBurnedBalance(getCakeAddress())) // TODO: change it to use helper that gets our token Address
+  const givePrice = usePriceCakeBusd()
+  const burnedBalance = getBalanceNumber(useBurnedBalance(getGiveAddress()))
+  const circSupply = totalSupply ? totalSupply.minus(burnedBalance) : new BigNumber(0);
   const cakeSupply = totalSupply ? getBalanceNumber(totalSupply) - burnedBalance : 0
+  const marketCap = givePrice.times(circSupply);
+
+  const farm0 = useFarmFromPid(0)
+  let givePerBlock = CAKE_PER_BLOCK.toNumber() // backup use the number in config/index.ts
+  // right now givePerBlock is undefined? so just using backup for now TODO: fix this
+  if(farm0 && farm0.givePerBlock){
+    givePerBlock = new BigNumber(farm0.givePerBlock).div(new BigNumber(10).pow(18)).toNumber();
+  }
+
+
 
   return (
     <StyledCakeStats>
       <CardBody>
         <Heading scale="xl" mb="24px">
-          {t('Give Token Stats')}
+          {t('GIVE Stats')}
         </Heading>
         <Row>
-          <Text fontSize="14px">{t('Total GIVE Supply')}</Text>
+          <Text fontSize="14px">{t('Market Cap')}</Text>
+          <CardValue fontSize="14px" value={getBalanceNumber(marketCap)} decimals={0} prefix="$" />
+        </Row>
+        <Row>
+          <Text fontSize="14px">{t('Total Minted')}</Text>
           {cakeSupply && <CardValue fontSize="14px" value={cakeSupply} />}
         </Row>
         <Row>
-          <Text fontSize="14px">{t('Total GIVE Burned')}</Text>
+          <Text fontSize="14px">{t('Total Burned')}</Text>
           <CardValue fontSize="14px" decimals={0} value={burnedBalance} />
         </Row>
         <Row>
+          <Text fontSize="14px">{t( 'Circulating Supply')}</Text>
+          {cakeSupply && <CardValue fontSize="14px" value={cakeSupply} decimals={0} />}
+        </Row>
+        <Row>
           <Text fontSize="14px">{t('New GIVE/block')}</Text>
-          <CardValue fontSize="14px" decimals={0} value={20} />
+          <CardValue fontSize="14px" decimals={3} value={givePerBlock} />
         </Row>
       </CardBody>
     </StyledCakeStats>
